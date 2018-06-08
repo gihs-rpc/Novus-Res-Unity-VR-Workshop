14c3d321f685851aca3edc7ea00e30fb2d94b4f9
Javad Hamidi <JahmHV@gmail.com>
2018-06-08 17:21:01 +0930

    Added initial repository

---
+++ Test Project NR/Assets/Scripts/MyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MyScript : MonoBehaviour {
+
+    public float Hitpoints;
+    public float mySpeed = 5f;
+    public FirstPersonController myController;
+	// Use this for initialization
+	void Start () {
+        Hitpoints = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Hitpoints = Hitpoints - 1f;
+        //mycontroller.WalkSpeed = MySpeed;
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            print("I have quit");
+            Application.Quit();
+
+        }
+
+    }
+}
